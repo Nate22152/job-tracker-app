85c929916ea08f5558325bdb1491ed825769a1c8
Nate22152 <104044540+Nate22152@users.noreply.github.com>
2025-12-28 20:09:52 -0600

    changed server.js to be compatible

--- my-job-tracker/server/server.js
+++ my-job-tracker/server/server.js
@@ -65,4 +65,7 @@ app.post('/login', async (req, res) => {
 app.use('/jobs', jobRoutes);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is live on port ${PORT}`);
+});
